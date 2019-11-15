1a03c7268fcf397afd845715760287edb1877ed4
bloeys <bloeys@outlook.com>
2019-11-15 18:23:48 +0400

    Revert "Test"
    This reverts commit b2eb9121c63b8f324cf55452daf2663b583a6a52.

--- sample-pipelines/task-and-script/scripts/hello.sh
+++ sample-pipelines/task-and-script/scripts/hello.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-cat "repo/README.md"
-cat "repo/descriptor.md"
+echo "Hello!"
+echo "This is running from a script"
+echo "Printing 1 to 10..."
 
-# echo "Hello!"
-# echo "This is running from a script"
-# echo "Printing 1 to 10..."
+for x in $(seq 1 10);
+do
+  echo "$x"
+done
 
-# for x in $(seq 1 10);
-# do
-#   echo "$x"
-# done
-
-# echo "Finished printing!"
+echo "Finished printing!"
